34a49dfae0b74737929e85e51582698cff45ede2
Georgi Karamanev <karamanev@gmail.com>
2019-02-25 09:47:12 +0200

    Heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/smartlottery'));
+app.use(express.static('./dist/lottery'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname, '/dist/smartlottery'));
+res.sendFile(path.join(__dirname, '/dist/lottery'));
 });
 
 // Start the app by listening on the default Heroku port
