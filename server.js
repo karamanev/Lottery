e70a769c781dba27f90f72e93e73bf9cb1e8a30d
Georgi Karamanev <karamanev@gmail.com>
2019-02-26 14:23:44 +0200

    Routing

--- server.js
+++ server.js
@@ -7,10 +7,22 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/lottery'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname, '/dist/lottery'));
+app.get('/user*', function (req, res) {
+
+    res.sendFile(path.join(__dirname, '/dist/lottery'));
 });
+
+app.get('/admin', function (req, res) {
+
+    res.sendFile(path.join(__dirname, '/dist/lottery'));
+});
+
+app.get('/*', function (req, res) {
+
+    res.sendFile(path.join(__dirname, '/dist/lottery'));
+});
+
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
